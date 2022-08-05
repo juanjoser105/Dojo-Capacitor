47b52a45c9e5111782cca00b528986ed190aed91
Juan José Ramirez Acosta <juan.ramirez105@udea.edu.co>
2022-08-04 22:17:05 -0500

    Dojocheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Dialog } from '@capacitor/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'capApp';
+
+  async showAlert(){
+    await Dialog.alert({
+      title: 'Atención',
+      message: 'Esto es una alertaa',
+    });
+  }
 }
